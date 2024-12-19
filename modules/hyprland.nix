2e69e5941a79f083d7c9ab3837d5dc414830b649
noah nguyen <nnoahnguyenn@gmail.com>
2024-12-19 15:46:33 -0500

    some typos

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -38,13 +38,13 @@
 	shadow = {
 	  enabled = true;
 	  range = 4;
-	  render_power = 3;
 	  color = "rgba(1a1a1aee)";
 	};
 	blur = {
 	  enabled = true;
 	  size = 3;
 	  passes = 1;
+	  render_power = 3;
 	  vibrancy = 0.1696;
 	};
       };
