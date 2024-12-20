3178c524d48557004ba5e762be7947d8f9c91c07
noah nguyen <nnoahnguyenn@gmail.com>
2024-12-20 13:03:06 -0500

    changed typos

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -4,25 +4,31 @@
     enable = true;
     package = pkgs.waybar;
 
-    settings = [{ 
-      layer = "top";
-      position = "top";
-      modules-left = [ "hyprland/workspaces" "hyprland/mode" ];
-      modules-center = [ "hyprland/window" ];
-      modules-right = [ "battery" "clock" ];
+    settings = {
+      mainBar = {
+        layer = "top";
+	position = "top";
+	height = 30;
+	output = [
+	  "eDP-1"
+	];
+	modules-left = [ "hyprland/workspaces" "hyprland/mode" ];
+	modules-center = [ "hyprland/window" ];
+	modules-right = [ "battery" "clock" ];
 
-      "hyprland/window" = {
-      format = "{}";
-      max-length = 50;
-      };
+	"hyprland/window" = {
+	format = "{}";
+	max-length = 50;
+	};
 
-      "battery" = {
-      "format" = "{capacity}$";
-      };
+	"battery" = {
+	"format" = "{capacity}$";
+	};
 
-      "clock" = {
-      format = "{:%a %d. %b  %I:%M %p}";
+	"clock" = {
+	format = "{:%a %d. %b  %I:%M %p}";
+	};
       };
-    }];
+    };
   }; 
 }
