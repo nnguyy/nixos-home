96d6decfcc9fb4f57eadfb10f62e858a72ebe5e4
noah nguyen <nnoahnguyenn@gmail.com>
2024-12-20 08:56:02 -0500

    typos

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -4,30 +4,32 @@
     enable = true;
 
     settings = {
+    mainBar = {
         layer = "top";
 	position = "top";
 	output = [
 	  "eDP-1"
 	];
 
-      modules-left = [ "hyprland/workspaces" "hyprland/mode" ];
-      modules-center = [ "hyprland/window" ];
-      modules-right = [ "battery" "clock" ];
+        modules-left = [ "hyprland/workspaces" "hyprland/mode" ];
+        modules-center = [ "hyprland/window" ];
+        modules-right = [ "battery" "clock" ];
 
-      "hyprland/window" = {
-        max-length = 50;
+        "hyprland/window" = {
+          max-length = 50;
+        };
+
+        "battery" = {
+          "format" = "{capacity}% {icon}";
+	  "format-icons" = "['1','2','3','4','5']";
+        };
+
+        "clock" = {
+          format = "{:%a %d. %b  %I:%M %p}";
+        };
+
+        "timezone" = "America/New_York";
       };
-
-      "battery" = {
-        "format" = "{capacity}% {icon}";
-	"format-icons" = "['1','2','3','4','5']";
-      };
-
-      "clock" = {
-        format = "{:%a %d. %b  %I:%M %p}";
-      };
-
-      "timezone" = "America/New_York";
     };
   };
 }
